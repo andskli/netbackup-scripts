ef7129c2113d4dc7ab1ac3bddc13adab212ae14e
ondmagi <andreas.skarmutsoslindh@gmail.com>
2013-12-11 17:37:05 +0100

    removing & from subroutine calls

--- excludelistmgr.pl
+++ excludelistmgr.pl
@@ -81,32 +81,32 @@ sub get_clients_in_policy
 		{
 			@p = split /\s+/, $_;
 			push(@out, $p[1]);
-			&debug(1, "found client $p[1] in $policyname");
+			debug(1, "found client $p[1] in $policyname");
 		}
 	}
 	return @out;
 }
 
 # Select what to get from bpgetconfig
-# &get_excludes("xyz.abc.com")
+# get_excludes("xyz.abc.com")
 sub get_excludes
 {
 	$client = $_[0];
 	$type = "EXCLUDE";
-	&debug(1, "Calling: $bpgetconfigbin -M $client $type");
+	debug(1, "Calling: $bpgetconfigbin -M $client $type");
 	my @output = `$bpgetconfigbin -M $client $type`;
 	return @output;
 }
 
 # Write excludelist to tempfile
-# &make_tempfile(\@excludes)
+# make_tempfile(\@excludes)
 # Returns path to tempfile
 sub make_tempfile
 {
 	my (@excludes) = @{$_[0]};
 	my $tmp = `mktemp`;
 	chomp $tmp;
-	&debug(1, "Created: $tmp");
+	debug(1, "Created: $tmp");
 
 	open(FH, ">>$tmp") or die "Can't open $tmp: $!";
 	foreach (@excludes)
@@ -118,7 +118,7 @@ sub make_tempfile
 	return $tmp;
 }
 
-# &push_excludes($client, $excludetmpfile)
+# push_excludes($client, $excludetmpfile)
 sub push_excludes
 {
 	my $client = $_[0];
@@ -138,7 +138,7 @@ sub main
 	}
 	if ($opt{'p'}) # if -p, we specify a policy
 	{
-		foreach (&get_clients_in_policy($opt{'p'}))
+		foreach (get_clients_in_policy($opt{'p'}))
 		{
 			push(@clients, $_);
 		}
@@ -161,7 +161,7 @@ sub main
 
 		foreach (@filedata)
 		{
-			&debug(1, "Found row containing [".$_."] in $opt{'f'}");
+			debug(1, "Found row containing [".$_."] in $opt{'f'}");
 			push(@excludes, "EXCLUDE = ".$_."\n");
 		}
 	}
@@ -171,8 +171,8 @@ sub main
 	{
 		foreach $client (@clients)
 		{
-			&debug(1, "processing $client");
-			my @client_excludes = &get_excludes($client);
+			debug(1, "processing $client");
+			my @client_excludes = get_excludes($client);
 			print "Excludes for client $client:\n";
 			foreach (@client_excludes)
 			{
@@ -187,7 +187,7 @@ sub main
 		{
 			# Fetch existing client excludes and push them into @excludes list
 			my @new_excludes;
-			my @existing = &get_excludes($client);
+			my @existing = get_excludes($client);
 			foreach $exclude (@existing)
 			{
 				push(@new_excludes, "$exclude\n");
@@ -196,9 +196,9 @@ sub main
 			{
 				push(@new_excludes, $_);
 			}
-			&uniq(@new_excludes);
-			my $f = &make_tempfile(\@new_excludes);
-			&push_excludes($client, $f);
+			uniq(@new_excludes);
+			my $f = make_tempfile(\@new_excludes);
+			push_excludes($client, $f);
 			push(@tmpfiles, $f);
 			undef(@new_excludes);
 		}
@@ -208,9 +208,9 @@ sub main
 	{
 		foreach $client (@clients)
 		{
-			&uniq(@excludes);
-			my $f = &make_tempfile(\@excludes);
-			&push_excludes($client, $f);
+			uniq(@excludes);
+			my $f = make_tempfile(\@excludes);
+			push_excludes($client, $f);
 			push(@tmpfiles, $f);
 		}
 	}
@@ -228,7 +228,7 @@ sub main
 	# Cleanup tempfiles
 	foreach my $f (@tmpfiles)
 	{
-		&debug(1, "Trying to delete [".$f."]");
+		debug(1, "Trying to delete [".$f."]");
 		unlink $f or warn "Could not unlink $f: $!";
 	}
 }
